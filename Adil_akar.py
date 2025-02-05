a6bfda012a695c2d0f58ffd684f3a92c0c98b942
Adil <alrizkyadil@gmail.com>
2025-02-05 21:32:56 +0700

    Add files via upload

---
+++ Adil_akar.py
@@ -0,0 +1,22 @@
+# Program mencari akar suatu bilangan
+# Programmer : Adil Al Rizky MK
+# Tanggal = 05/02/2025 
+
+# mengakses fungsi matematika
+import math
+
+# Meminta Input dari Pengguna
+angka = float(input("Masukan angka: "))
+
+# Pengecekan Bilangan Negatif
+if angka < 0:
+    
+    # Menampilan pernyataan 
+    print("Angka tidak boleh negatif")
+
+# Menghitung Akar Kuadrat
+else:
+    Akar_Kuadrat = math.sqrt(angka)
+    
+    # Menampilkan hasil 
+    print(f"Akar kuadrat dari {angka} adalah {Akar_Kuadrat}")
